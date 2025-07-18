0e7692970ea8327579234db2c67326e86df9f4b9
nandan kumar <nandanbharadwaj4@gmail.com>
2025-07-18 22:35:38 +0530

    added models for many to many assoc between roles and users

---
+++ src/migrations/20250718165143-create-user-role.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('User_Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users', // Assuming you have a Users table
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'roles', // Assuming you have a Roles table
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('User_Roles');
+  }
+};
